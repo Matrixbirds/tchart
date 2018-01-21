668f13504ae752eca61003f52f5985abb3c0a4e8
matrixbirds <yooobuntu@163.com>
2018-01-21 23:20:48 +0800

    first bloodgst

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as d3 from 'd3';
+const h = 360
+const w = 480;
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
---
+++ webpackfile.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
